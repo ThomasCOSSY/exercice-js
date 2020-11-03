1afb20f297d684ca6b7a8708c23712d990a9fe9c
ThomasCOSSY <thomascossy888@gmail.com>
2020-11-03 10:52:04 +0100

    partie 4

--- Partie 4/src/exercicesAnswers.js
+++ Partie 4/src/exercicesAnswers.js
@@ -5,65 +5,85 @@ let createArray = function () {
 
 //Exercice 2 : Retouner le 3ème élément du tableau days', function() {
 let returnThirdElement = function (array) {
-  return array.split(days).reverse(Mercredi).join(days);
+  return array[2];
 }
 
 //Exercice 3 : Afficher la valeur de l\'index 3 du tableau days.', function() {
 let returnThirdIndex = function (array) {
-  return 'A compléter';
+  return array[3];
 }
 
 //Exercice 4 : Changer le 2ème élément du tableau languages par php.', function() {
 let changeSecondElement = function (array) {
-  return 'A compléter';
+  array.splice( 1, 1, 'php')
+  return array;
+  //splice pour intégrer un element, premier chiffre la position (0=1element, 1=2element ect...), deuxeme chiffre 0 on rajoute un index, 1 on modifie un element
 }
 
 //Exercice 5 : Ajouter ruby et python à la fin du tableau languages.', function() {
 let addAtTheEnd = function (array) {
-  return 'A compléter';
+  array.push('ruby', 'python')
+  return array;
 }
 
 //Exercice 6 : Ajouter html et css au début du tableau languages.', function() {
 let addAtFirst = function (array) {
-  return 'A compléter';
+  array.unshift('html', 'css')
+  return array;
 }
 
 //Exercice 7 : Supprimer le premier élément du tableau languages.', function() {
 let removeFirstElement = function (array) {
-  return 'A compléter';
+  array.shift()
+  return array;
 }
 
 //Exercice 8 : Supprimer le dernier élément du tableau languages.', function() {
 let removeLastElement = function (array) {
-  return 'A compléter';
+  array.pop()
+  return array;
 }
 
 //Exercice 9 : Créer un tableau associatif person. Lui ajouter les index name, age et city avec les valeurs Jean, 45, Paris.', function() {
 let createPerson = function () {
-  return 'A compléter';
+  let person = [];
+  person['name'] = 'Jean';
+  person['age'] = 45;
+  person['city'] = 'Paris';
+  return person;
 }
 
 //Exercice 10 : Retourner la valeur de l\'index nom du tableau person.', function() {
 let returnName = function (array) {
-  return 'A compléter';
+  let person = [];
+  person['name'] = 'Jean';
+  return person['name'];
 }
 
 //Exercice 11 : Stocker tous les index du tableau person dans des variables séparées et les retourner dans une même phrase de type : "Bonjour, je suis Jean. J\'ai 45 ans et j\'habite Paris."', function() {
 let returnSentence = function (array) {
-  return 'A compléter';
+  let person = [];
+  let name = person['name'] = 'Jean';
+  let age = person['age'] = 45;
+  let city = person['city'] = 'Paris';
+  return "Bonjour, je suis " + name +  ". J\'ai " + age + " ans et j\'habite "+ city + ".";
 }
 
 //Exercice 12 : Transformer la variable string en tableau.', function() {
 let returnArray = function (string) {
-  return 'A compléter';
+  let array = string.split([',']);
+  // creation d un tableau array + split/séparation des mots contenue dnas string en se basant sur les virgules
+  return array;
 }
 
 //Exercice 13 : Trier le tableau array.', function() {
 let sortArray = function (array) {
-  return 'A compléter';
+  array.sort()
+  return array;
 }
 
 //Exercice 14 : Transformer le tableau array en chaîne de caractères', function() {
 let returnString = function (array) {
-  return 'A compléter';
+  let string = array.toLocaleString();
+  return string;
 }
